4812ccf5737b2b8ea355f60699a9776373027eec
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-04-12 11:20:26 +0300

    next commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// const swiper = new Swiper(".materials__slider", {
-//   loop: true,
-//   autoplay: {
-//     delay: 5000
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true
-//   }
-// });
-
-// window.addEventListener("scroll", () => {
-//   const header = document.querySelector(".header");
-//   header.classList.toggle("sticky", window.scrollY > 0);
-// });
-
 const button = document.getElementById("btn");
 
 button.addEventListener("click", () => {
